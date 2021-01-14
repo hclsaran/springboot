a2686df9f1e85876c65f0a7d1278fad18afd95ca
hclsaran <renusaran@hotmail.com>
2021-01-13 19:27:31 -0500

    Update SpringBootSwaggerApplication.java

--- SpringBootSwagger/src/main/java/com/hcl/SpringBootSwaggerApplication.java
+++ SpringBootSwagger/src/main/java/com/hcl/SpringBootSwaggerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 public class SpringBootSwaggerApplication {
-
+//comment
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSwaggerApplication.class, args);
 	}
